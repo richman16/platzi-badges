76cab1e1e0189b39d771885d6a049e15c245140f
Ricardo Castillo <itic.ricardocastillo@gmail.com>
2019-12-02 02:42:32 -0600

    3. Componentes - Implementacion de manejo de estados (componentes controlados)

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
+    state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        jobTitle: 'Designer',
+        twitter: '',
+    };
+
     handleChange = (event) => {
         console.log({ 
             name: event.target.name,
             value: event.target.value,
+        });
+
+        this.setState({
+            [event.target.name]: event.target.value,
         });
     };
 
@@ -15,6 +27,7 @@ class BadgeForm extends React.Component {
     handleSubmit = (event) => {
         event.preventDefault();
         console.log("Form was submitted");
+        console.log(this.state);
     }
 
     render () {
@@ -24,7 +37,53 @@ class BadgeForm extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label>First name:</label>
-                        <input onChange={this.handleChange} className="form-control" type="text" name="firstName"/>
+                        <input
+                            onChange={this.handleChange}
+                            className="form-control"
+                            type="text"
+                            name="firstName"
+                            value={this.state.firstName}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Last name:</label>
+                        <input
+                            onChange={this.handleChange}
+                            className="form-control"
+                            type="text"
+                            name="lastName"
+                            value={this.state.lastName}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Email:</label>
+                        <input
+                            onChange={this.handleChange}
+                            className="form-control"
+                            type="email"
+                            name="email"
+                            value={this.state.email}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Job title:</label>
+                        <input
+                            onChange={this.handleChange}
+                            className="form-control"
+                            type="text"
+                            name="jobTitle"
+                            value={this.state.jobTitle}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Twitter:</label>
+                        <input
+                            onChange={this.handleChange}
+                            className="form-control"
+                            type="text"
+                            name="twitter"
+                            value={this.state.twitter}
+                        />
                     </div>
                     <button
                         onClick = {this.handleClick}
